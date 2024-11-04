591653bbde58261a9097fdce829e3509484af9b9
Arman-Saini <arman.saini.13.06.04@gmail.com>
2024-11-04 22:50:28 +0530

    MERA git ni chalda :cryEmoji git da beda behje

--- middleware/roleMiddleware.js
+++ middleware/roleMiddleware.js
@@ -2,4 +2,6 @@ exports.authorizeRole = (role) => (req, res, next) => {
     if (req.user.role !== role) return res.status(403).send("Access denied.");
     next();
   };
+
+  //adding just to check
   
